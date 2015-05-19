94ec86ad0fc9458ff9c5de433dd1e6d731fcda82
YUKI Hiroshi <shimoda@clear-code.com>
2015-05-19 16:49:42 +0900

    Add parser for load's response

---
+++ test/groonga/response/load.test.js
@@ -0,0 +1,16 @@
+var assert = require('chai').assert;
+
+var LoadResponse = require('../../../lib/groonga/response/load');
+
+suite('Groonga response for column_create', function() {
+  var source = [
+    [0, 1431504631.3374963, 0.0036923885345458984],
+    29
+  ];
+
+  test('loadedCount', function() {
+    var response = new LoadResponse(source);
+    assert.equal(29, response.loadedCount);
+  });
+});
+
