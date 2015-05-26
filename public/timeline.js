ed8d38a642b8a17549c4f34083f763b3576a2ca8
YUKI Hiroshi <shimoda@clear-code.com>
2015-05-26 18:47:31 +0900

    Insert new events with animation effect

--- public/timeline.js
+++ public/timeline.js
@@ -4,7 +4,11 @@ function checkNewEvents() {
     url: '/timeline-new-events?boundary=' + boundary
   }).done(function(data) {
     var newEvents = $(data);
+    newEvents.each(function(newEvent) {
+      newEvents.css({ display: 'none' });
+    });
     $('#events').prepend(newEvents);
+    newEvents.animate({ height: 'show', opacity: 'show' }, 'normal');
     setTimeout(checkNewEvents, 3000);
   });
 }
