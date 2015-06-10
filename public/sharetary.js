6a97df1ec49a0b596367f28ef5cd2bd6dc058226
YUKI Hiroshi <shimoda@clear-code.com>
2015-06-10 19:18:58 +0900

    Extract operations to fill datetime fields to the common script file

--- public/sharetary.js
+++ public/sharetary.js
@@ -15,7 +15,33 @@ function scrollToEvent(link) {
     scrollToTarget(id);
 }
 
+
+function fillDatetimeFields(prefix) {
+  var filterValue = $('#filter-' + prefix).attr('value');
+  if (filterValue) {
+    var date = new Date(parseInt(filterValue) * 1000);
+    $('#filter-' + prefix + '-year').attr('value', date.getFullYear());
+    $('#filter-' + prefix + '-month').attr('value', date.getMonth() + 1);
+    $('#filter-' + prefix + '-date').attr('value', date.getDate());
+    $('#filter-' + prefix + '-hour').attr('value', date.getHours());
+    $('#filter-' + prefix + '-minute').attr('value', date.getMinutes());
+    $('#filter-' + prefix + '-second').attr('value', date.getSeconds());
+  }
+  else {
+    $('#filter-' + prefix + '-year').attr('value', '');
+    $('#filter-' + prefix + '-month').attr('value', '');
+    $('#filter-' + prefix + '-date').attr('value', '');
+    $('#filter-' + prefix + '-hour').attr('value', '');
+    $('#filter-' + prefix + '-minute').attr('value', '');
+    $('#filter-' + prefix + '-second').attr('value', '');
+  }
+}
+
+
 $(function() {
+  fillDatetimeFields('after');
+  fillDatetimeFields('before');
+
   var id = encodeURIComponent(location.hash.substr(1));
   if (id)
     scrollToTarget(id);
