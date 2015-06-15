6a32f543f3465b935b7194dcf102d8447655ca7d
YUKI Hiroshi <shimoda@clear-code.com>
2015-06-15 11:14:52 +0900

    Use single input field in formatted datetime text to specify datetime filters

--- public/sharetary.js
+++ public/sharetary.js
@@ -20,20 +20,22 @@ function fillDatetimeFields(prefix) {
   var filterValue = $('#filter-' + prefix).val();
   if (filterValue) {
     var date = new Date(parseInt(filterValue) * 1000);
-    $('#filter-' + prefix + '-year').val(date.getFullYear());
-    $('#filter-' + prefix + '-month').val(date.getMonth() + 1);
-    $('#filter-' + prefix + '-date').val(date.getDate());
-    $('#filter-' + prefix + '-hour').val(date.getHours());
-    $('#filter-' + prefix + '-minute').val(date.getMinutes());
-    $('#filter-' + prefix + '-second').val(date.getSeconds());
+    $('#filter-' + prefix + '-format').val(
+      ('000' + date.getFullYear()).substr(-4) +
+      '-' +
+      ('0' + (date.getMonth() + 1)).substr(-2) +
+      '-' +
+      ('0' + date.getDate()).substr(-2) +
+      ' ' +
+      ('0' + date.getHours()).substr(-2) +
+      ':' +
+      ('0' + date.getMinutes()).substr(-2) +
+      ':' +
+      ('0' + date.getSeconds()).substr(-2)
+    );
   }
   else {
-    $('#filter-' + prefix + '-year').val('');
-    $('#filter-' + prefix + '-month').val('');
-    $('#filter-' + prefix + '-date').val('');
-    $('#filter-' + prefix + '-hour').val('');
-    $('#filter-' + prefix + '-minute').val('');
-    $('#filter-' + prefix + '-second').val('');
+    $('#filter-' + prefix + '-format').val('');
   }
 }
 
@@ -43,16 +45,24 @@ function updateFilterFromFields(prefix) {
     window.clearTimeout(updateFilterFromFields.timers[prefix]);
   updateFilterFromFields.timers[prefix] = window.setTimeout(function() {
     delete updateFilterFromFields.timers[prefix];
-    var date = new Date();
-    date.setFullYear(parseInt($('#filter-' + prefix + '-year').val() || 0),
-                     parseInt($('#filter-' + prefix + '-month').val() || '1') - 1,
-                     parseInt($('#filter-' + prefix + '-date').val() || 1));
-    date.setHours(parseInt($('#filter-' + prefix + '-hours').val() || 0),
-                  parseInt($('#filter-' + prefix + '-minute').val() || 0),
-                  parseInt($('#filter-' + prefix + '-second').val() || 0),
-                  0);
-    var unixtime = Math.floor(date.getTime() / 1000);
-    $('#filter-' + prefix).val(unixtime);
+    var formatDatetime = $('#filter-' + prefix + '-format').val();
+    var parsed = formatDatetime.match(/^\s*(\d+)-(\d+)-(\d+)(?:\s*(\d+):(\d+):(\d+))?/);
+    if (parsed) {
+      var year   = parseInt(parsed[1] || '0');
+      var month  = parseInt(parsed[2] || '1') - 1;
+      var date   = parseInt(parsed[3] || '1');
+      var hour   = parseInt(parsed[4] || '0');
+      var minute = parseInt(parsed[5] || '0');
+      var second = parseInt(parsed[6] || '0');
+      var filter = new Date();
+      filter.setFullYear(year, month, date);
+      filter.setHours(hour, minute, second, 0);
+      var unixtime = Math.floor(filter.getTime() / 1000);
+      $('#filter-' + prefix).val(unixtime);
+    }
+    else {
+      $('#filter-' + prefix).val('');
+    }
   }, 100);
 }
 
